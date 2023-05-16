4eca1326c6aea7e55e326e3a3cafd4ab62b54057
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-05-16 21:08:24 +0300

    image delete done

--- file_manger/app/components/ImagesArray/index.jsx
+++ file_manger/app/components/ImagesArray/index.jsx
@@ -1,11 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import {
-  storage,
-  uploadBytes,
-  ref,
-  getDownloadURL,
-  arrayUnion,
+  arrayRemove,
   db,
   updateDoc,
   doc,
@@ -26,12 +22,29 @@ export default function ImagesArray() {
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-      // console.log(doc.data());
       setImages(() => {
         return doc.data().images;
       });
-      console.log(images);
     });
+  };
+
+  const deleteImageHandler = async (url) => {
+    const q = query(
+      collection(db, "users"),
+      where("uid", "==", localStorage.getItem("user"))
+    );
+
+    const querySnapshot = await getDocs(q);
+    let documentID;
+    querySnapshot.forEach((doc) => {
+      documentID = doc.id;
+    });
+    const userRef = doc(db, "users", documentID);
+
+    await updateDoc(userRef, {
+      images: arrayRemove(url),
+    });
+    setImages(images.filter((image) => image !== url));
   };
 
   useEffect(() => {
@@ -52,7 +65,12 @@ export default function ImagesArray() {
               <a href="image">
                 <img src={image} alt="img" />
               </a>
-              <button className="text-white bg-stone-700 hover:bg-stone-800 focus:ring-4 focus:outline-none focus:ring-stone-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-stone-600 dark:hover:bg-stone-700 dark:focus:ring-stone-800">
+              <button
+                onClick={(e) => {
+                  deleteImageHandler(image);
+                }}
+                className="text-white bg-stone-700 hover:bg-stone-800 focus:ring-4 focus:outline-none focus:ring-stone-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-stone-600 dark:hover:bg-stone-700 dark:focus:ring-stone-800"
+              >
                 Delete Image
               </button>
             </div>
