3f65ff5b4e4f5db0fdf136d8d68e74c5b602136e
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-05-16 21:11:01 +0300

    done what's required for the task

--- file_manger/app/components/Folder/index.jsx
+++ file_manger/app/components/Folder/index.jsx
@@ -8,11 +8,11 @@ export default function Folder({ path, d, name }) {
   return (
     <div
       onClick={() => {
-        navigate.push({ path });
+        navigate.push(path);
       }}
       className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700"
     >
-      <p>{name}</p>
+      {/* <p>{name}</p> */}
       <svg
         className="w-12 h-12 text-gray-200"
         xmlns="xhttp://www.w3.org/2000/svg"
